77b0080fb2209ec32505db11e3a5ea3a2842eb37
zika2112 <24.damt.cesaralejandroramos@fundacionmontessori.com>
2025-05-24 21:48:35 +0200

    agrego la base sin hacer del test mago tes y se agrega un paquete de pruebas

---
+++ Practica4CesarRamos/src/pruebas/MagoTest.java
@@ -0,0 +1,14 @@
+package pruebas;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MagoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
